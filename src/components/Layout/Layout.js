ad283bb63eaf3c9731c114d0a314ac26f5fd96bb
Kamil Brzozowski <idlesoluszyn@gmail.com>
2019-04-04 16:30:10 +0200

    Google adsense

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -15,14 +15,6 @@ const Layout = (props) => {
                 <meta name="robots" content="index, follow" />
                 <link rel="shortcut icon" type="image/x-icon" href={icon}/>
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
-
-                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                <script>
-                (adsbygoogle = window.adsbygoogle || []).push({{
-                    google_ad_client: "ca-pub-6859884578348905",
-                    enable_page_level_ads: true
-                }});
-                </script>
             </Helmet>
             <Nav />
             {props.children}
